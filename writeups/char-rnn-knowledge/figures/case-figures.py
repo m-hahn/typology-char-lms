a11e9eb8e4757e4be642e3f01de381777e3a7b97
Michael Hermann Hahn <mhahn@fb.com>
2018-09-01 19:03:34 +0200

    figures

---
+++ writeups/char-rnn-knowledge/figures/case-figures.py
@@ -0,0 +1,39 @@
+
+accLSTM = [[0.88, 0.93], [0.96, 0.76], [0.94, 0.89], [0.99, 0.7]]
+
+accNgram = [[0.94, 0.95], [0.5, 0.5], [0.5, 0.5], [0.5, 0.5]]
+
+accRNN = [[0.93, 0.61], [0.82, 0.46], [0.76, 0.33], [1.0, 0.0]]
+
+accWord = [[0.97, 0.98], [0.98, 0.94], [1.0, 0.75], [1.0, 0.71]]
+
+
+
+names = ["CNLM", "N-Grams", "RNN CNLM", "Word LSTM"]
+accs = [accLSTM, accNgram, accRNN, accWord]
+
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+for gender, genderName in zip(range(2), ["Dative", "Genitive"]):
+  for name, acc in zip(names, accs):
+   plt.plot(range(0, 4), [x[gender] for x in acc], label=name, linewidth=4.0)
+  plt.xticks(range(0,4))
+  plt.legend()
+  plt.show()
+  plt.savefig("german-case-"+genderName+".pdf")
+  plt.close()
+
+
+for name, acc in zip(names, accs):
+ plt.plot(range(0, 4), [sum(x)/2 for x in acc], label=name, linewidth=4.0)
+plt.xticks(range(0,4))
+plt.legend()
+plt.show()
+plt.savefig("german-case-total.pdf")
+plt.close()
+
---
+++ writeups/char-rnn-knowledge/figures/gender-figures.py
@@ -0,0 +1,44 @@
+
+accLSTM = [[[0.7, 0.21, 0.09], [0.06, 0.9, 0.03], [0.2, 0.21, 0.59]],
+           [[0.63, 0.2, 0.17], [0.15, 0.78, 0.07], [0.31, 0.21, 0.49]],
+           [[0.67, 0.19, 0.14], [0.16, 0.76, 0.07], [0.31, 0.22, 0.48]]]
+
+accNgram = [[[0.85, 0.08, 0.07], [0.39, 0.57, 0.04], [0.12, 0.08, 0.8]],
+            [[0.34, 0.32, 0.34], [0.32, 0.34, 0.34], [0.33, 0.33, 0.34]],
+            [[0.33, 0.33, 0.34], [0.34, 0.34, 0.33], [0.32, 0.34, 0.34]]]
+
+accRNN = [[[0.38, 0.47, 0.15], [0.08, 0.88, 0.05], [0.22, 0.45, 0.33]],
+          [[0.2, 0.64, 0.15], [0.11, 0.79, 0.09], [0.19, 0.65, 0.16]],
+          [[0.23, 0.54, 0.23], [0.19, 0.55, 0.26], [0.21, 0.55, 0.24]]]
+
+accWord = [[[0.77, 0.15, 0.08], [0.05, 0.92, 0.03], [0.11, 0.14, 0.74]],
+           [[0.74, 0.11, 0.14], [0.06, 0.88, 0.06], [0.14, 0.12, 0.74]],
+           [[0.83, 0.07, 0.1], [0.09, 0.87, 0.04], [0.18, 0.11, 0.71]]]
+
+names = ["CNLM", "N-Grams", "RNN CNLM", "Word LSTM"]
+accs = [accLSTM, accNgram, accRNN, accWord]
+
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+for gender, genderName in zip(range(3), ["m", "f", "n"]):
+  for name, acc in zip(names, accs):
+   plt.plot(range(0, 3), [x[gender][gender] for x in acc], label=name, linewidth=4.0)
+  plt.xticks(range(0,3))
+  plt.legend()
+  plt.show()
+  plt.savefig("german-gender-"+genderName+".pdf")
+  plt.close()
+
+for name, acc in zip(names, accs):
+ plt.plot(range(0, 3), [sum([x[gender][gender] for gender in range(3)])/3 for x in acc], label=name, linewidth=4.0)
+plt.xticks(range(0,3))
+plt.legend()
+plt.show()
+plt.savefig("german-gender-total.pdf")
+plt.close()
+  
